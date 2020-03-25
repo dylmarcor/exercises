c9f30ce033c7eebd10b9bd35cb6a1aebc5590f99
Dylmarcor <dylancorbett@icloud.com>
2020-03-25 14:53:39 -0700

    modify two_sum and add linked_list into js

--- js/two_sum.js
+++ js/two_sum.js
@@ -6,6 +6,9 @@
     solution, and you may not use the same element twice.
 */
 
+
+// My first version - Comment out one of the functions to run.
+
 let twoSum = function(nums, target) {
     let one = 0, two = 0, test = 0, len = nums.length + 1;
     let arr = [];
@@ -21,4 +24,21 @@ let twoSum = function(nums, target) {
     }
 };
 
-twoSum([3, 2, 4], 6);
+// Version found utilizing the Map object - most effecient
+// var twoSum = function(nums, target) {
+//     let map = new Map;
+//     for (let i = 0; i < nums.length; i++) {
+//         let complement = target - nums[i];
+//         if (map.has(complement)) {
+//             return [map.get(complement), i]
+//         }
+//         map.set(nums[i], i)
+//     }
+// };
+
+if (twoSum([2, 7, 10, 12], 9) == [0, 1]) {
+    console.log("Function correct");
+}
+if (twoSum([3, 2, 4], 6) == [1, 2]) {
+    console.log("Function correct");
+}
