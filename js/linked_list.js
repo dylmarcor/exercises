662c113faa9c1d7c2b0da98cc12a062cea75ec49
Dylmarcor <dylancorbett@icloud.com>
2020-03-25 18:26:12 -0700

    remove function added to SinglyList proto

--- js/linked_list.js
+++ js/linked_list.js
@@ -59,3 +59,44 @@ SinglyList.prototype.searchNodeAt = function(position) {
 
     return currentNode;
 }
+
+// Function to remove Node from list at position given
+
+SinglyList.prototype.remove = function(position) {
+    var currentNode = this.head,
+	length = this._length,
+	count = o,
+	message = {failure: 'Failure: non-existent node in this list.'},
+	beforeNodeToDelete = null,
+	nodeToDelete = null,
+	deletedNode = null;
+
+    // 1st use case - an invalid position
+    if (position < 0 || position > length) {
+	throw new Error(message.failure);
+    }
+
+    // 2nd use case - the first Node is removed
+    if (position === 1) {
+	this.head = currentNode.next;
+	deletedNode = currentNode;
+	currentNode = null;
+	this._length--;
+
+	return deletedNode;
+    }
+
+    // 3rd use case - any other Node is removed
+    while (count < position) {
+	beforeNodeToDelete = currentNode;
+	nodeToDelete = currentNode.next;
+	count++;
+    }
+
+    beforeNodeToDelete.next = nodeToDelete.next;
+    deletedNode = nodeToDelete;
+    nodeToDelete = null;
+    this._length--;
+
+    return deletedNode;
+};
