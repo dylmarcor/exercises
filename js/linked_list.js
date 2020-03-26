4bcb01ad47df4a1005bb872b6931912d3428d32a
Dylmarcor <dylancorbett@icloud.com>
2020-03-25 18:01:12 -0700

    add logic on SinglyList proto

--- js/linked_list.js
+++ js/linked_list.js
@@ -4,3 +4,34 @@ function Node(data) {
     this.data = data;
     this.next = null;
 };
+
+// Make SinglyList data type for Single-Linked-List
+
+function SinglyList() {
+    this._length = 0;
+    this.head = null;
+}
+
+SinglyList.prototype.add = function(value) {
+    var node = new Node(value),
+	currentNode = this.head;
+
+    // 1st use case - empty list
+    if (!currentNode) {
+	this.head = node;
+	this._length++;
+
+	return node;
+    }
+
+    // 2nd use case - a non-empty list
+    while (currentNode.next) {
+	currentNode = currentNode.next;
+    }
+
+    currentNode.next = node;
+
+    this._length++;
+
+    return node;
+};
