3f7b70029e3bb6757711f1db8e28c091f06d952f
Dylmarcor <dylancorbett@icloud.com>
2020-03-30 15:47:50 -0700

    add map function to result

--- js/reverse_int.js
+++ js/reverse_int.js
@@ -6,7 +6,22 @@
  */
 
 let reverse = function(x) {
+    // if (Number.isNaN(x)) {
+    //     return "Not a number!";
+    // }
     if (x < ((-21)**31) || x > (21**31 -1)) {
         return "Larger int than 32-bit."
     }
+
+    let y = (""+x).split("");
+    let arr = [];
+    let result = Number;
+    for (let i = 0; i < y.length; i++) {
+        arr.unshift(y.pop());
+    }
+    result = a.map((x) => {
+        return parseInt(x, 10);
+    }); 
+    
+    return result;
 };
